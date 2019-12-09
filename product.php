690b47898ebf268c8435aa347f76cf3c079ad318
Mukesh1991 <mukeshmehta25@gmail.com>
2019-12-09 11:11:06 +0530

    update product page changes
    update product page changes

--- product.php
+++ product.php
@@ -23,7 +23,7 @@
                     <div class="htc__service__wrap">
                        
                         <!-- Start Single Service -->
-                        <div class="col-md-4 col-lg-4 col-sm-6 col-xs-12">
+                        <!--<div class="col-md-4 col-lg-4 col-sm-6 col-xs-12">
                             <div class="service foo">
                                 <div class="service__thumb">
                                     <a href="service.html">
@@ -35,11 +35,11 @@
                                     <p>What are foods? Read about Fats, Fatty acids, Trans Fatty Acids, Proteins, Amino Acids, Carbohydrates, Phenolics, Aroma and Taste</p>
                                 </div>
                             </div>
-                        </div>
+                        </div>-->
                         <!-- End Single Service -->
                         
                         <!-- Start Single Service -->
-                        <div class="col-md-4 col-lg-4 col-sm-6 col-xs-12">
+                        <!--<div class="col-md-4 col-lg-4 col-sm-6 col-xs-12">
                             <div class="service foo">
                                 <div class="service__thumb">
                                     <a href="service.html">
@@ -51,11 +51,11 @@
                                     <p>Many compounds have been shown to have positive antioxidants effects . Read about Carotenoids, Vitamin E, Resveratrol, Epicatechin, Vitamin C, Proanthocyanidins...</p>
                                 </div>
                             </div>
-                        </div>
+                        </div>-->
                         <!-- End Single Service -->
                         
                         <!-- Start Single Service -->
-                        <div class="col-md-4 col-lg-4 col-sm-6 col-xs-12">
+                        <!--<div class="col-md-4 col-lg-4 col-sm-6 col-xs-12">
                             <div class="service foo">
                                 <div class="service__thumb">
                                     <a href="service.html">
@@ -67,7 +67,7 @@
                                     <p>What is a food supplement? Read about the B vitamins, Vitamin D, Quercetin, Resveratrol, Vitamin E, Vitamin K, Vitamin C, Herbs and Botanicals, Amino Acids ...</p>
                                 </div>
                             </div>
-                        </div>
+                        </div>-->
                         <!-- End Single Service -->
                         
                         
@@ -77,6 +77,76 @@
                         
                        
                     </div>
+                    
+                    
+                    <div class="product_name_table">
+                        <table class="table table-bordered">
+                            <thead>
+                              <tr>
+                                <th>TABLETS</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>1. Coenzyme Q10 100mg, Lycopene(10%) 5000mcg, L-carnitine 500mg, L-arginine 100mg, Zinc Sulphate 12mg and Selenium 40mcg Tablets</td>
+                                
+                              </tr>
+                              <tr>
+                                <td>2. Coenzyme Q10 100MG, Omega 3 Fatty Acids, Methylcobalamin 500mcg, Lycopene(10%) 5000mcg,  Zinc and Selenium Tablets </td>
+                              
+                              </tr>
+                              <tr>
+                                <td>3. Glucosamine 750mg, Boswellia Serrata(65%) 200mg, Methyl Sulfonyl Methane 200mg, Chondroitin Sul-phate 100mg, Curcumin 50mg, Hyaluronic Acid 10mg, Magnesium Oxide 10mg, Zinc 7.5mg Tablets</td>
+                               
+                              </tr>
+                              
+                              
+                                <tr>
+                                <td>4. Rosehip Extract 100mg, Glucosamine 750mg, Boswellia Serrata(65%) 200mg, Methyl Sulfonyl Methane 200mg, Chondroitin Sulphate 100mg, Curcumin 50mg, Hyaluronic acid 10mg, Vit K2-7 22.5mcg, Vit D3 400 IU,  Zinc Sulphate 7.5mg Tablets</td>
+                               
+                              </tr>
+                              
+                              
+                                <tr>
+                                <td>5. Rutin 100mg, Quercetin 100mg, Glucosamine 750mg, Methyl Sulfonyl Methane 200mg, Tablets</td>
+                               
+                              </tr>
+                              
+                              
+                                <tr>
+                                <td>6. Rosehip Extract 250mg, Collagen Peptide(II) 40mg, Sodium Hyalornate 30mg, Chonoroitin Sulphate So-dium 200mg and Vitamin C 35mg Tablets </td>
+                               
+                              </tr>
+                              
+                                <tr>
+                                <td>7. Calcium Citrate Malate 1250mg eq to 250mg of Elemental Calcium, Calcitrol 0.25mcg, Vitamin k2-7 22.5mcg, Zinc Sulphate Monohydrate 20mg and Boron 500mcg Tablets</td>
+                               
+                              </tr>
+                              
+                                <tr>
+                                <td>8. Calcium Citrate Malate 1250mg eq to 250mg of Elemental Calcium, Fructo Oligosaccharides 200mg,  Vit D3 100 IU, Vitamin k2-7 22.5mcg, Zinc 15mg, Magnesium 12.85mg, Copper 1.5mg, Manganese 12.85mg Tablets</td>
+                               
+                              </tr>
+                              
+                              
+                              <tr>
+                                <td>9. Calcium Citrate Malate 1250mg eq to 250mg of Elemental Calcium, Vitamin D3 100 IU, Folic Acid 50mcg Tablets					</td>
+                               
+                              </tr>
+                              
+                              
+                              <tr>
+                                <td>10. Calcium Citrate Malate 1250mg eq to Elemental Calcium  250mg, Soy Isflavones (40% ) 20mg,                
+Ipriflavone 200mg, Fructooligosaccharides 200mg, Vitamin D3  5 mcg, Magnesium 12.85mg ,Copper 1mg,              Zinc Sulphate 10mg, Manganese  2.5mg, Vitamin K2-7  22.5mcg                     
+</td>
+                               
+                              </tr>
+                            </tbody>
+                          </table>
+                    </div>
+                    
+                    
+                    
                 </div>
             </div>
         </section>
