3ba55f76c265b91ff52454414b240923053c2075
arvind-webtech <developer.webtechexpers@gmail.com>
2019-12-09 17:19:22 +0530

    Updated Product Table
    Updated Product Table

--- product.php
+++ product.php
@@ -17,9 +17,78 @@
         </div>
         <!-- End Bradcaump area -->
         <!-- Start Service Area -->
+        
+        
+        <div class="product-slider">
+                            <div class="bradcaump__inner text-center">
+                                <h2 class="bradcaump-title inner-slide">Tablets</h2>
+                            </div>
+        <div class="container">
+               
+               
+                 <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="2000">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                          <li data-target="#myCarousel" data-slide-to="1"></li>
+                          <li data-target="#myCarousel" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+
+
+                          <div id="thumbcarousel" class="carousel slide" data-interval="false">
+                        <div class="carousel-inner">
+                                           <div class="item active">
+                                                <div data-target="#carousel" data-slide-to="0" class="thumb"><img src="images/products/tab-3.jpg"></div>
+                                                <div data-target="#carousel" data-slide-to="1" class="thumb"><img src="images/products/tab-2.jpg"></div>
+                                                <div data-target="#carousel" data-slide-to="2" class="thumb"><img src="images/products/tab-1.jpg"></div>
+                                                <div data-target="#carousel" data-slide-to="3" class="thumb"><img src="images/products/tab-3.jpg"></div>
+                                            </div><!-- /item -->
+                                            <div class="item">
+                                                <div data-target="#carousel" data-slide-to="4" class="thumb"><img src="images/products/tab-2.jpg"></div>
+                                                <div data-target="#carousel" data-slide-to="5" class="thumb"><img src="images/products/tab-3.jpg"></div>
+                                                <div data-target="#carousel" data-slide-to="6" class="thumb"><img src="images/products/tab-1.jpg"></div>
+                                                <div data-target="#carousel" data-slide-to="7" class="thumb"><img src="images/products/tab-2.jpg"></div>
+                                            </div><!-- /item -->
+                                            <div class="item">
+                                                <div data-target="#carousel" data-slide-to="5" class="thumb"><img src="images/products/tab-3.jpg"></div>
+                                                <div data-target="#carousel" data-slide-to="6" class="thumb"><img src="images/products/tab-1.jpg"></div>
+                                                <div data-target="#carousel" data-slide-to="4" class="thumb"><img src="images/products/tab-2.jpg"></div>
+                                                <div data-target="#carousel" data-slide-to="7" class="thumb"><img src="images/products/tab-2.jpg"></div>
+                                            </div><!-- /item -->
+                        </div><!-- /carousel-inner -->
+                    </div> <!-- /thumbcarousel -->
+
+
+
+                        </div>
+
+  
+              </div>
+                <div class="clearfix">
+                
+               
+                
+                </div><!-- /clearfix -->
+
+
+
+
+
+            
+            
+        </div>
+        </div>
+        
+        
+        
         <section class="htc__service__area service--page bg__gray ptb--120">
             <div class="container">
                 <div class="row">
+                   
+                   <!---
                     <div class="htc__service__wrap">
                        
                         <!-- Start Single Service -->
@@ -67,16 +136,16 @@
                                     <p>What is a food supplement? Read about the B vitamins, Vitamin D, Quercetin, Resveratrol, Vitamin E, Vitamin K, Vitamin C, Herbs and Botanicals, Amino Acids ...</p>
                                 </div>
                             </div>
-                        </div>-->
-                        <!-- End Single Service -->
+                        </div>
                         
                         
-                        
-                       
                         
                         
                        
-                    </div>
+                        
+                        
+                       
+                    </div>--><!-- End Single Service -->
                     
                     
                     <div class="product_name_table">
@@ -140,7 +209,105 @@
 Ipriflavone 200mg, Fructooligosaccharides 200mg, Vitamin D3  5 mcg, Magnesium 12.85mg ,Copper 1mg,              Zinc Sulphate 10mg, Manganese  2.5mg, Vitamin K2-7  22.5mcg                     
 </td>
                                
+                              </tr> 
+                              <tr>
+                                <td>11. Calcium Carbonate 500mg, Calcitrol 0.25mcg, Vitamin k2-7  22.5mcg, Zinc 20mg and Boron 500mcg Tablets              
+                                </td>
+                               
                               </tr>
+                              
+                              <tr>
+                                <td>12. Calcium Carbonate 500mg, Methylcobalamin, Vit D3 100IU, Vitamin k2-7  22.5mcg, Zinc 20mg and 
+Boron 500 mg Tablets</td>
+                               
+                              </tr>
+                              
+                              <tr>
+                                <td>13. Carica Papaya Extract 250mg, Tinospora Cordifolia Extract 150mg, Goat Milk Powder 50mg, Vitamin E
+ 40mg Tablets</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>14. Grape Seed Extract, Soya Protein Isolate, Minerals, Amino Acids and Multivitamin Tablets</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>15. Ferrous Bisglycinate 300mg eq to Elemental Iron 60mg, Methylcobalamin 500mcg, Folic Acid 1mg an
+Zinc Sulphate 15mg Tablets</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>16. Ferrous Ascorbate eq to Elemental Iron 100mg, Folic acid, Vit B12</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>17. Ferric Ammonium Citrate, Folic Acid and Zinc Sulphate Tablets</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>18. Silymarin Extract, l-Ornithine, l-Glutamine N-Acetyl Cysteine, Co- Enzyme Q-10, Inositol and
+Antioxidants Tablets</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>19. Ginkgo Biloba Extract 60mg, Vit E 200mg Tablets</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>20. Lycopene(10%) 5000mcg, Lutein 250mcg, Vit A 1500 IU, Ascorbic Acid 10mg, Vit D3 100 IU, Vit E 10mg, Thiamin 3mg, Riboflavin 1mg, Pyridoxin 10mcg, Vit B12 500mcg, Niacinamide 25mg, Calcium D-Pantothenate 5mg, Folic acid 1.5mg, Ferrous Ascorbate 20mg, Magnesium Oxide 10mg, Manganese Sul-phate 5mg, Copper Sulphate 1mcg, Zinc 25mg, Selenium 100mcg, Chromium Chloride 100mcg, L- Glutamic Acid 10mg, N-Acetyl Cystein 10mg, Biotin 30mcg,  Tablets</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>21. Cranberry Extract 200mg, D-Mannose 200mg, Lactobacillus Acidophilus 150million, Lactobacillus  Salivarius 150millions, Lactobacillus Rhamnous 150millions, Fructo Oligosaccharides 50mg, Vit A 5000 IU, Vit C 50mg Tablets</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>22. L-Methylfolate 1000mcg Tablets</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>23. Beta Carotene 30mg, Zinc 27.5mg, Selenium 200mcg, Manganese 2mg, Copper Sulphate 1mg </td>
+                               
+                              </tr>
+                              <tr>
+                                <td>24. Collagen Petides (Type 1)  75mg,  Resveratrol 5mg, Grape Seed Extract 50mg, Green Tea Extract 25mg, Pine Bark Extract 20mg, D-Salina Extract (Contain Natural Mixed Carotenoids) 15mg, Aloe Vera Extrac 10mg, Pomegranate Extract 10mg, Lycopene (10%) 5000 mcg, Astaxanthin 5%  12mg, Vitamin C 15mg, Vitamin E 15 IU, Biotin  5mg,Vitamin B6   0.75mg, Zinc 7.5mg, L-Glutathione 5mg, Soy Flavonoids 20mg, Nicotinamide 5mg, Konjac Powder 10mg, Leuocotomos Extract  25mg Tablets</td>
+                               
+                              </tr>
+                              
+                              <tr>
+                                <td>25. Myo-inositol 550mg, D-Chiro-inositol 13.80mg, Green tea extract 200mg, Vit D3 200IU, Chromium Picolinate 100mcg Tablets</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>26. Methylcobalmin 1500mcg, Chromium Picolinate, Vit A, Vit C, Vit D3, Selenium Tablets</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>27. Omega 3 Fatty Acids, Methylcobalmin, Calcium Citrate Malate, Cholecalciferol, Folic acid, Boron Tablets</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>28. (Zincovit Tab) Grape Seed Extract 50mg,Vit C 40mg, Vit B3 18mg, Vit E 10mg, Zinc 10mg, Magnsium
+3mg, Vit B5 3mg, Vit B2 1.6mg, Vit B1 1.4mg, Vit B6 1mg, Vit A 600mcg, Manganese 250mcg, Biotin
+150mcg, Folic Acid 100mcg, Iodine 100mcg, Copper 30mcg, Selenium 30mcg, Chromium 25mcg, Vit D3
+5mcg, Vit B12 1mcg</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>29. Methylcobalamin 500 mcg, Lycopene(10%) 5000mcg,  Ginseng Extract Powder(10%) 42.5mg, Ginkgo
+Biloba Extract 5mg, GreenTea Extract 10mg,  Grape Seed Extract 5mg, Vitamin A 2500 IU, Vitamin D3
+200 IU, Vitamin E(50%) 25 IU, Vitamin C 50mg, Zinc Sulphate 5mg, Copper Sulphate 2mg, Sodium
+Chloride 40mcg, Calcium Picolinate 5mg, Chromium Picolinate 200mcg, Lysin 10mg, Folic Acid 1.5mg,
+Calcium 5mg, Phosphate 3.86 mg, Selenium 70 mcg, Potassium Chloride 10mg, Manganese 5mg,  Vitamin
+B1 1mg, Vitinam B6 1mg Tablets</td>
+                               
+                              </tr>
+                              <tr>
+                                <td>30.Garcinia Cambogia, Green Tea extract, Ginger and Piperine tablets </td>
+                               
+                              </tr>
+                              
+                              
                             </tbody>
                           </table>
                     </div>
